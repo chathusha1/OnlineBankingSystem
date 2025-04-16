60765e51a96ce64914ad18d0b52b916c1f31e25a
Chathusha Dinuranga <chathubrooit@gmail.com>
2025-04-16 16:37:03 +0530

    Update DBConnection.java

--- src/service/DBConnection.java
+++ src/service/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/online_banking";
     private static final String USER = "root";
-    private static final String PASSWORD = "32213221";
+    private static final String PASSWORD = "12345678";
 
     
     public static Connection getConnection() throws SQLException {
